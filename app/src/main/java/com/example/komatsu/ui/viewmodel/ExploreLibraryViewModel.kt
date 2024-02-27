34a3d978b3bb1a0677f21aba04e3133bd2d34eb4
paschyz <paschy2002@gmail.com>
2024-02-27 18:07:41 +0100

    Add ExploreLibraryViewModel

---
+++ app/src/main/java/com/example/komatsu/ui/viewmodel/ExploreLibraryViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.komatsu.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+
+class ExploreLibraryViewModel : ViewModel() {
+    private val _index = MutableLiveData<Int>()
+    val text: LiveData<String> = _index.map {
+        "Hello world from section: $it"
+    }
+
+    fun setIndex(index: Int) {
+        _index.value = index
+    }
+}
