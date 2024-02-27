cf5977476efbefe1385cd83678c74b0d615aa16d
paschyz <paschy2002@gmail.com>
2024-02-27 18:06:11 +0100

    SectionsPagerAdapter and PageViewModel files added

--- app/src/main/java/com/example/komatsu/di/modules/AppModule.kt
+++ app/src/main/java/com/example/komatsu/di/modules/AppModule.kt
@@ -1,4 +1,14 @@
 package com.example.komatsu.di.modules
 
-class AppModule {
+import com.example.komatsu.domain.repository.MangaRepository
+import com.example.komatsu.ui.viewmodel.MangaDetailsViewModel
+import com.example.komatsu.ui.viewmodel.MangaListViewModel
+import com.example.komatsu.ui.di.AppModule
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val AppModule = module {
+    single { MangaRepository(get()) }
+    viewModel { MangaDetailsViewModel(get()) }
+    viewModel { MangaListViewModel(get()) }
 }
---
+++ app/src/main/java/com/example/komatsu/ui/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.example.komatsu.ui.di
+
+class AppModule {
+}
